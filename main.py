b91d364ab6054b8779acca5ba8c0debe8e2e209e
turgalibauyrzhan <turgalibauyrzhan@gmail.com>
2025-12-15 10:24:02 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from telegram.ext import (
 # =========================
 # НАСТРОЙКИ
 # =========================
-TOKEN = os.environ.get("8293279514:AAEcTtUeB9kXaLn3viOVty7jzFgboAC1l8Q")
+TOKEN = os.environ.get("8293279514:AAGxWpkxeHQAoCzKM9KPYWefyKR4GlKxl20")
 if not TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN is not set (Render env var).")
 
