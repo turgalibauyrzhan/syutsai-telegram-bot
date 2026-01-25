1327fa20b3b72a27cc28cc662f37eb6bd6079916
turgalibauyrzhan <turgalibauyrzhan@gmail.com>
2026-01-25 20:36:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("BOT_TOKEN:", bool(BOT_TOKEN))
+print("BOT_TOKEN:", bool(TELEGRAM_TOKEN))
 print("PUBLIC_URL:", bool(PUBLIC_URL))
 print("SHEET_ID:", bool(SHEET_ID))
 print("GOOGLE_CREDS_JSON:", bool(GOOGLE_CREDS_JSON))
